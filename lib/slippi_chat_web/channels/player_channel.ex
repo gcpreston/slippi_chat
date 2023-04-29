8eea99a943d858d904fd72f8878f7fb6b78dffdd
Graham Preston <graham.preston@gmail.com>
2023-04-29 11:56:32 -0700

    Forward session start/end to channel

--- lib/slippi_chat_web/channels/player_channel.ex
+++ lib/slippi_chat_web/channels/player_channel.ex
@@ -4,16 +4,22 @@ defmodule SlippiChat.PlayerChannel do
   require Logger
 
   alias SlippiChat.ChatSessionRegistry
+  alias SlippiChat.ChatSessions.ChatSession
 
+  @impl true
   def join("players:" <> player_code, _payload, socket) do
     ChatSessionRegistry.register_client(ChatSessionRegistry, player_code)
+    Phoenix.PubSub.subscribe(SlippiChat.PubSub, "chat_sessions:#{player_code}")
+
     {:ok, socket |> assign(:client_code, player_code)}
   end
 
+  @impl true
   def terminate(_reason, socket) do
     ChatSessionRegistry.remove_client(ChatSessionRegistry, socket.assigns.client_code)
   end
 
+  @impl true
   def handle_in("game_started", %{"players" => player_codes}, socket)
       when is_list(player_codes) do
     ChatSessionRegistry.game_started(
@@ -28,5 +34,19 @@ defmodule SlippiChat.PlayerChannel do
   def handle_in("game_ended", _params, socket) do
     ChatSessionRegistry.game_ended(ChatSessionRegistry, socket.assigns.client_code)
     {:reply, :ok, socket}
+  end
+
+  @impl true
+  def handle_info({[:session, :start], {players, pid}}, socket) do
+    {:ok, uuid} = ChatSession.get_uuid(pid)
+    Phoenix.PubSub.subscribe(SlippiChat.PubSub, "chat_sessions:#{uuid}")
+    push(socket, "session_start", players)
+
+    {:noreply, socket}
+  end
+
+  def handle_info({[:session, :end], {players, _pid}}, socket) do
+    push(socket, "session_end", players)
+    {:noreply, socket}
   end
 end
