ccfb6cab92b9116a44546c05bfd94048baaa2506
Graham Preston <graham.preston@gmail.com>
2025-02-24 01:11:40 +0100

    Basic token gen interface

--- lib/slippi_chat_web/live/create_token_live.ex
+++ lib/slippi_chat_web/live/create_token_live.ex
@@ -3,11 +3,52 @@ defmodule SlippiChatWeb.CreateTokenLive do
 
   def render(assigns) do
     ~H"""
-    Create token placeholder: <%= @current_user_code %>
+    <div class="mx-auto max-w-md">
+      <.simple_form for={@form} id="grant-form" phx-submit="create" phx-update="ignore">
+        <.input field={@form[:grantee]} label="Connect code" required />
+
+        <:actions>
+          <.button phx-disable-with="Creating..." class="w-full">
+            Create
+          </.button>
+        </:actions>
+      </.simple_form>
+
+      <div :if={@new_token} class="mt-4 text-center">
+        <div class="text-zinc-600">New token created, copy it now, you won't be able to again:</div>
+        <div id="new-token"><%= @new_token %></div>
+        <span>
+          <.button
+            onclick="copyToClipboard()"
+            class="mt-2 display-block"
+            phx-click={JS.remove_class("hidden", to: "#check")}
+          >Copy</.button>
+          <span id="check" class="hidden"><.icon id="check" name="hero-check-solid" /></span>
+        </span>
+      </div>
+
+      <script>
+        function copyToClipboard() {
+          const copyTextElem = document.getElementById("new-token");
+          navigator.clipboard.writeText(copyTextElem.innerHTML);
+        }
+      </script>
+    </div>
     """
   end
 
+  def handle_event("create", %{"grantee" => grantee}, socket) do
+    granted_token = "test_token" # Auth.generate_admin_client_token(grantee)
+
+    {:noreply, socket |> assign(new_token: granted_token)}
+  end
+
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    form = to_form(%{"grantee" => nil})
+    {:ok,
+      socket
+      |> assign(form: form)
+      |> assign(new_token: nil),
+      temporary_assigns: [form: form]}
   end
 end
