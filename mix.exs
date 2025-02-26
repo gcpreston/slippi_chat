3232761791cc68b36faec52d7929a8483b91fc53
Graham Preston <graham.preston@gmail.com>
2025-02-26 16:12:44 +0100

    Elixir 1.18.2, OTP 27.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SlippiChat.MixProject do
     [
       app: :slippi_chat,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
